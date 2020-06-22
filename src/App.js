7a5569e8aa277940a9f796b8a943efb585105f4e
gormeshinyan <gormeshinyan@mail.ru>
2020-06-22 15:08:09 +0400

    Install "Redux" and "react-redux"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'; 
 import './App.css';
+import { useSelector , useDispatch } from 'react-redux';
 
 function App() {
+  const dispatch=useDispatch();
+  const counter=useSelector((state)=>state.counter);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Counter : {counter}</h1>
+        <button onClick={(()=>dispatch({type:'INCREMENT'}))} >INCREMENT</button>
+        <button onClick={(()=>dispatch({type:'RESET'}))} >RESET</button>
+        <button onClick={(()=>dispatch({type:'DECREMENT'}))} >DECREMENT</button>
       </header>
     </div>
   );
 }
+
 
 export default App;
